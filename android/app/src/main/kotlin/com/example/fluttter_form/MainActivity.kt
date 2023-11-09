e09fcd6c49c5668684dadc184ffc1e899569d42b
Manoj Santra <santramanoj1997@gmail.com>
2023-11-10 01:58:55 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/fluttter_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttter_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,25 @@
+import 'package:fluttter_form/models/category.dart';
+import 'package:flutter/material.dart';
+
+import 'package:fluttter_form/models/category.dart';
+import 'package:fluttter_form/data/categories.dart';
+import 'package:fluttter_form/models/grocery_item.dart';
+
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
